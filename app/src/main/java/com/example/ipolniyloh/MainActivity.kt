fa437e9157b635757a18c620ff1a8594ce49388b
XcidRxin <bogdanognevoy@gmail.com>
2022-04-22 10:04:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ipolniyloh/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ipolniyloh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val l = findViewById<TextView>(R.id.text)
+        val loh = findViewById<Button>(R.id.but)
+        val loh1 = findViewById<Button>(R.id.button2)
+        val loh2 = findViewById<Button>(R.id.button3)
+        var polniy = 0
+        var i = 11111*4343343
+        loh.setOnClickListener {
+            polniy++
+            l.text = polniy.toString()
+        }
+        loh1.setOnClickListener {
+            polniy -= 100
+            l.text = polniy.toString()
+        }
+        loh2.setOnClickListener {
+            polniy += i
+            l.text = polniy.toString()
+        }
+
+    }
+}
